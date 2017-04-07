b8bcef2dc140802ed3e4fafc323f8bc3c4e905dd
zhaiqianfeng <zhaiqianfeng@163.com>
2017-04-07 11:22:11 +0800

    update

---
+++ gradle/test-dependencies-artifacts/test-artifact/src/main/java/Library.java
@@ -0,0 +1,5 @@
+public class Library {
+	public String sayHello() {
+		return "hello,gradle!";
+	}
+}
---
+++ gradle/test-dependencies-artifacts/test-dependency/src/main/java/App.java
@@ -0,0 +1,8 @@
+//import com.zqf.Library;
+
+public class App {
+    public static void main(String[] args) {
+    	Library library=new Library();
+    	System.out.println(library.sayHello());
+    }
+}
