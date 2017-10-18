be6c121ee9471a8275ef3f0b0887367ef9835d90
Chris <chris@chris.zhai>
2017-10-18 10:42:23 +0800

    update

--- spring/spring-cloud/cloud-zuul/src/main/java/com/example/cloudzuul/ZuulApplication.java
+++ spring/spring-cloud/cloud-zuul/src/main/java/com/example/cloudzuul/ZuulApplication.java
@@ -2,11 +2,13 @@ package com.example.cloudzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
-public class DemoApplication {
+public class ZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ZuulApplication.class, args);
 	}
 }
